6043a8b68dcb10b9f53c1d06945d9a8d12fc2e4a
Anny He <anny.he@salesforce.com>
2019-09-29 17:01:02 -0700

    - add to READme - simplify EJS to plain html

--- READme.md
+++ READme.md
@@ -1,7 +1,22 @@
 # Real time chat app using Socket.io, Nodejs
 
-- No database. Considering sqlite for testing. Store username and message
+- Database: use sqlite for testing. Stores username and messages
+
+TODO
 - Extend to make it more slack-like. Ie. @inspiration to get inspirational message, or @imageCat to get image of a cat from unsplash
+- convert jQuery on the front to React
 - UI can be better, ie. make it more obvious who is talking. Anyone other than the current person should be in a separate column
 - no react
 - no tests
+
+### Learned
+
+**How to create tables in Sqlite3 npm package**
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS ChatMessages(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, message text)",
+  function() {
+    console.log("successful");
+  }
+);
+
--- index.js
+++ index.js
@@ -1,28 +1,63 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const sqlite3 = require("sqlite3").verbose();
+const path = require('path');
+const DB_PATH = "./sqliteChatBot.db";
 
-app.get('/', function(req, res) {
-    res.render('index.ejs');
+let db = new sqlite3.Database(DB_PATH, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the " + DB_PATH + " SQlite database.");
+}); 
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+    //__dirname : It will resolve to your project folder. 
+//   res.render("./views/index.html");
 });
 
-io.sockets.on('connection', function(socket) {
-    socket.on('username', function(username) {
-        socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
-    });
+io.sockets.on("connection", function(socket) {
+  socket.on("username", function(username) {
+    socket.username = username;
+    io.emit("is_online", "🔵 <i>" + socket.username + " join the chat..</i>");
+  });
 
-    socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
-    })
+  socket.on("disconnect", function() {
+    io.emit("is_online", "🔴 <i>" + socket.username + " left the chat..</i>");
+  });
 
-    socket.on('chat_message', function(message) {
-        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
-    });
-
+  socket.on("chat_message", function(message) {
+    io.emit(
+      "chat_message",
+      "<strong>" + socket.username + "</strong>: " + message
+    );
+    db.run(
+      `INSERT INTO ChatMessages(username, message) VALUES(?, ?)`,
+      [socket.username, message],
+      function(error) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Insert successful. # of Row Changes: " + this.changes);
+        }
+      }
+    );
+  });
 });
 
 const server = http.listen(8080, function() {
-    console.log('listening on *:8080');
+  console.log("listening on *:8080");
+});
+
+process.on("SIGINT", () => {
+  db.close(err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Close the database connection.");
+  });
+  server.close();
 });
